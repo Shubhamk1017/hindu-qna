519fffb787202e6115c5c20cc21602a1ac9aa1d2
Shubhamk1017 <yadavshubham50102@gmail.com>
2026-05-30 18:22:50 +0530

    Fix FiX import for build

--- client/src/pages/AdminPanel.js
+++ client/src/pages/AdminPanel.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../utils/api';
 import { useAuth } from '../context/AuthContext';
-import { FiUsers, FiMessageSquare, FiTag, FiSettings, FiCheck, FiCpu } from 'react-icons/fi';
+import { FiUsers, FiMessageSquare, FiTag, FiSettings, FiCheck, FiX, FiCpu } from 'react-icons/fi';
 import toast from 'react-hot-toast';
 
 const AdminPanel = () => {
